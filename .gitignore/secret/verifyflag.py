cda6776bef78b73574ec671e6340178ea6a8f53c
mystic707 <86408697+seventyoseven@users.noreply.github.com>
2025-10-30 08:30:45 +0400

    Update verifyflag.py

--- .gitignore/secret/verifyflag.py
+++ .gitignore/secret/verifyflag.py
@@ -1,4 +1,4 @@
 # Secret validation module (keep private)
 def check_flag(user_flag: str) -> bool:
-    correct_flag = "FLAG{this_is_the_real_one}"  # hidden here
+    correct_flag = "flag{shifted_message}"  # hidden here
     return user_flag == correct_flag
